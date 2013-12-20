21be6c9288907964b73b5d13517c26f09714b08a
Jon Phenow <j.phenow@gmail.com>
2013-12-20 14:11:25 -0600

    drop in mysql tweaks, depend on mysql cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             'sportngin-mysql-cookbook'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Sport Ngin, Inc.'
+maintainer_email 'infrastructure@sportngin.com'
 license          'All rights reserved'
 description      'Installs/Configures sportngin-mysql-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "mysql", ">= 4.0.0"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,24 @@
-#
-# Cookbook Name:: sportngin-mysql-cookbook
-# Recipe:: default
-#
-# Copyright (C) 2013 YOUR_NAME
-# 
-# All rights reserved - Do Not Redistribute
-#
+package "yum-utils"
+
+mysql_release_rpm = "/var/chef/cache/mysql-community-release.rpm"
+cookbook_file "mysql-community-release.rpm" do
+  path mysql_release_rpm
+  notifies :run, "execute[add-mysql-package-repo]", :immediately
+end
+
+execute "add-mysql-package-repo" do
+  command "yum -y localinstall #{mysql_release_rpm}"
+  action :nothing
+  notifies :run, "execute[enable-mysql55]", :immediately
+end
+
+execute "enable-mysql55" do
+  command "yum-config-manager --disable mysql56-community && yum-config-manager --enable mysql55-community"
+  action :nothing
+  notifies :run, "execute[update-yum]", :immediately
+end
+
+execute "update-yum" do
+  command "yum -y clean all"
+  action :nothing
+end
