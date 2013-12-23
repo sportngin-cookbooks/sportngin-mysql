6fb090b57885356afef0dbabaabae8426cab7e05
Andy Fleener <andrew.fleener@sportngin.com>
2013-12-22 20:47:43 -0600

    install the shared and client packages as well

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,6 @@ default['mysql']['server']['percona']['version'] = '5.5'
 default['mysql']['server']['percona']['patch_level'] = '28'
 default['mysql']['server']['percona']['release'] = '29.3'
 default['mysql']['server']['percona']['tag'] = '388'
+default['mysql']['server']['percona']['packages'] = ['shared','client','server']
 
 override['mysql']['server']['packages'] = []
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,15 +2,17 @@ version = node[:mysql][:server][:percona][:version]
 patch_level = node[:mysql][:server][:percona][:patch_level]
 release = node[:mysql][:server][:percona][:release]
 tag = node[:mysql][:server][:percona][:tag]
-package_path = "#{Chef::Config[:file_cache_path]}/Percona-Server-server-#{version}.#{patch_level}-rel#{release}.rpm"
+
+node[:mysql][:server][:percona][:packages].each do |package|
+package_path = "#{Chef::Config[:file_cache_path]}/Percona-Server-#{package}-#{version}.#{patch_level}-rel#{release}.rpm"
 
 
 cookbook_file package_path do
-    source "Percona-Server-server-#{version.gsub(".","")}-#{version}.#{patch_level}-rel#{release}.#{tag}.rhel6.x86_64.rpm"
+    source "Percona-Server-#{package}-#{version.gsub(".","")}-#{version}.#{patch_level}-rel#{release}.#{tag}.rhel6.x86_64.rpm"
     action :create
 end
 
-package "Percona-Server-server-#{version.gsub(".","")}" do
+package "Percona-Server-#{package}-#{version.gsub(".","")}" do
     source package_path
     action :install
 end
