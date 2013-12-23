ee3cf9851d24d661dd6964a9a26d9cd0db10f1e5
Andy Fleener <andrew.fleener@sportngin.com>
2013-12-23 15:38:05 -0600

    Add back the chef dependency management

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 # Resides in Berksfile because it currently comes from Github
-#depends "mysql", ">= 4.0.0"
+depends "mysql", "4.0.5"
