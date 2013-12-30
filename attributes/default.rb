9465f5305aa71750da6cf5ede6e39c6e6ff28487
Andy Fleener <andrew.fleener@sportngin.com>
2013-12-30 15:38:03 -0600

    Add the mysql devel package, its required for the mysql2 gem

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ default['mysql']['server']['percona']['version'] = '5.5'
 default['mysql']['server']['percona']['patch_level'] = '28'
 default['mysql']['server']['percona']['release'] = '29.3'
 default['mysql']['server']['percona']['tag'] = '388'
-default['mysql']['server']['percona']['packages'] = ['shared','client','server']
+default['mysql']['server']['percona']['packages'] = ['shared','client','server', 'devel']
 
 override['mysql']['server']['service_name'] = 'mysql'
 override['mysql']['server']['packages'] = []
